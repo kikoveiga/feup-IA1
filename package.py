b850330067fee49535b5b1e41d6e4aad1c244a7f
Vidal322 <pedrovidal2004@gmail.com>
2024-03-25 20:07:05 +0000

    menu

--- package.py
+++ package.py
@@ -13,6 +13,25 @@ reputation_weight = 0.5
 num_packages = 10
 map_size = 60
 
+class icons:
+    TOP_LEFT = '\u250f'
+    TOP_RIGHT = '\u2513'
+    BOTTOM_LEFT = '\u2517'
+    BOTTOM_RIGHT = '\u251b'
+    VERTICAL = '\u2503'
+    HORIZONTAL = '\u2501'
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 class Package:
     def __init__(self, package_type, coordinates):
@@ -36,6 +55,8 @@ class Package:
     def distance(self, coordinates):
         return ((self.coordinates_x - coordinates[0])**2 + (self.coordinates_y - coordinates[1])**2)**0.5
 
+    def __repr__(self):
+        return f"Package('{self.package_type}', ({self.coordinates_x}, {self.coordinates_y}))"
 def is_damaged(package: Package, distance_covered: float) -> bool:
     p_damage = 1 - (1 - package.breaking_chance)**distance_covered
     return random.uniform(0, 1) < p_damage
@@ -58,21 +79,24 @@ def calculate_unexpected_cost(package: Package, distance_covered: float, penalty
     return cost
 
 
-def evaluate_solution(solution: list[int], packages: list[Package], cost_per_km: float, penalty_cost_per_minute: float) -> float:
+def evaluate_solution(solution: list[int]) -> float:
     total_cost: float = 0
     distance_covered: float = 0
     reputation_cost: float = 0
 
-    distance_covered += packages[solution[0]].distance((0, 0))
-    total_cost += distance_covered * cost_per_km + calculate_unexpected_cost(packages[solution[0]], distance_covered, penalty_cost_per_minute)
+    distance_covered += package_stream[solution[0]].distance((0, 0))
+    total_cost += distance_covered * cost_per_km + calculate_unexpected_cost(package_stream[solution[0]], distance_covered, penalty_cost_per_minute)
 
     for i in range(1, len(solution)):
-        distance_covered += packages[solution[i]].distance((packages[solution[i - 1]].coordinates_x, packages[solution[i - 1]].coordinates_y))
+        distance_covered += package_stream[solution[i]].distance((package_stream[solution[i - 1]].coordinates_x, package_stream[solution[i - 1]].coordinates_y))
         distance_cost = distance_covered * cost_per_km
-        reputation_cost += calculate_unexpected_cost(packages[solution[i]], distance_covered, penalty_cost_per_minute)
+        reputation_cost += calculate_unexpected_cost(package_stream[solution[i]], distance_covered, penalty_cost_per_minute)
         
         total_cost += (1 - reputation_weight) * distance_cost + reputation_weight * reputation_cost
     return total_cost
+
+# function to evaluate the cost of a neighbour solution more efficiently than evaluating the whole solution again
+#def evaluate_neighbour_solution(solution: list[int], cost: int, index_1: int, index_2: int) -> float:
 
 # hill climbing
 
@@ -86,22 +110,46 @@ def get_neighbor_solution(solution: list[int]) -> list[int]:
     return neighbor_solution
 
 
-def hill_climbing(packages: list[Package], cost_per_km: float, penalty_cost_per_minute: float, neighbor_function: Callable [[list[int]], list[int]], iterations: int = 1000) -> list[int]:
-    num_packages = len(packages)
+def hill_climbing(neighbor_function: Callable [[list[int]], list[int]], iterations: int = 1000) -> list[int]:
+
     best_solution = random.sample(range(num_packages), num_packages)
-    best_cost = evaluate_solution(best_solution, packages, cost_per_km, penalty_cost_per_minute)
+    best_cost = evaluate_solution(best_solution)
 
     for _ in range(iterations):
         solution = neighbor_function(best_solution)
-        cost = evaluate_solution(solution, packages, cost_per_km, penalty_cost_per_minute)
+        cost = evaluate_solution(solution)
 
         if cost < best_cost:
             best_cost = cost
             best_solution = solution
 
+    return best_solution, best_cost
+
+
+# simulated annealing
+
+def simulated_annealing(neighbor_function: Callable[[list[int]], list[int]], iterations: int = 1000, temperature: float = 1.0, cooling: float = 0.0) -> list[int]:
+    solution = generate_random_solution()
+    cost = evaluate_solution(solution)
+    best_solution = solution
+    best_cost = cost
+
+    for _ in range(iterations):
+        neighbor_solution = neighbor_function(solution)
+        neighbor_cost = evaluate_solution(neighbor_solution)
+        delta = cost - neighbor_cost
+        acceptance_probability = np.exp(delta / temperature)
+        
+        if neighbor_cost < cost or random.random() < acceptance_probability:
+            solution = neighbor_solution
+            cost = neighbor_cost
+
+        if cost < best_cost:
+            best_solution = solution
+            best_cost = cost
+
+        temperature *= 1 - cooling
     return best_solution
-
-
 
 
 
@@ -118,7 +166,6 @@ def pmx_crossover(solution_1, solution_2):
     crossover_index  = np.random.randint(1, len(solution_1))
     child_1 = copy.deepcopy(solution_1)
     child_2 = copy.deepcopy(solution_2)
-    print("Crossover Point: ", crossover_index)
     for i in range(crossover_index):
         if child_1[i] != solution_2[i]:
             change_index = np.where(child_1 == solution_2[i])[0][0]
@@ -140,7 +187,7 @@ def swap_mutation(solution):
     return solution
 
 
-def inversion_mutation(solution):
+def inversion_mutation(solution : list[int])-> list[int]:
     index_1 = np.random.randint(0, len(solution))
     index_2 = (index_1 + np.random.randint(0, len(solution))) % (len(solution) - 1)
     if index_1 > index_2:
@@ -148,7 +195,7 @@ def inversion_mutation(solution):
     solution[index_1:index_2] = solution[index_1:index_2][::-1]
     return solution
 
-def scramble_mutation(solution):
+def scramble_mutation(solution: list[int])-> list[int]: 
     index_1 = np.random.randint(0, len(solution))
     index_2 = (index_1 + np.random.randint(0, len(solution))) % (len(solution) - 1)
     if index_1 > index_2:
@@ -156,58 +203,75 @@ def scramble_mutation(solution):
     np.random.shuffle(solution[index_1:index_2])
     return solution
 
-def random_mutation(solution):
+def random_mutation(solution : list[int]) -> list[int]:
     mutation = np.random.choice([swap_mutation, inversion_mutation, scramble_mutation])
     return mutation(solution)
 
 # selection functions
 
 #tournament solution
-def greatest_fit(population: list[list[int]], packages: list[Package], cost_per_km: float, penalty_cost_per_minute: float):  # podemos ter de retornar a score para além da solução
+
+# returns the solution with highest scrore from a population
+def greatest_fit(population: list[list[int]]) -> list[int]:  # podemos ter de retornar a score para além da solução
     best_solution = population[0]
-    best_score = evaluate_solution(best_solution, packages, cost_per_km, penalty_cost_per_minute)
+    best_score = evaluate_solution(best_solution)
     for sol in population:
-        score = evaluate_solution(sol, packages, cost_per_km, penalty_cost_per_minute)
+        score = evaluate_solution(sol)
         if score > best_score:
             best_solution = sol
             best_score = score
     return best_solution
     
-
-def generate_population(population_size):
+# Returns a list of random solutions
+def generate_population(population_size :int)-> list[int]:
     solutions = []
     for _ in range(population_size):
         solutions.append(generate_random_solution())
     return solutions
 
-def tournament_selection(population, packages, cost_per_km, penalty_cost_per_minute, tournament_size=20):
+# Randomly chooses tournament_size solutions from the population and returns the one with the highest scores
+def tournament_selection(population :list[list[int]], tournament_size:int = 20):
     if len(population) < tournament_size:
-        return greatest_fit(population, packages, cost_per_km, penalty_cost_per_minute)
+        return greatest_fit(population)
     participants = random.sample(population, tournament_size)
-    return greatest_fit(participants, packages, cost_per_km, penalty_cost_per_minute)
+    return greatest_fit(participants)
     
 # roulette wheel selection
-def roulette_wheel_selection(population: list[list[int]], packages: list[Package], cost_per_km: float, penalty_cost_per_minute: float):
-    scores = [evaluate_solution(sol, packages, cost_per_km, penalty_cost_per_minute) for sol in population]
+def roulette_wheel_selection(population: list[list[int]]) -> list[int]:
+    scores = [evaluate_solution(sol) for sol in population]
     total_score = sum(scores)
     probabilities = [score / total_score for score in scores]
     return population[np.random.choice(len(population), p=probabilities)]
 
-def random_selection(population, packages, cost_per_km, penalty_cost_per_minute):
+
+# Randomly chooses between tournament selection and roulette wheel selection
+def random_selection(population : list[list[int]], tournament_size = 20) -> list[int]:
     selection_function = np.random.choice([tournament_selection, roulette_wheel_selection])
     if selection_function == tournament_selection:
-        return selection_function(population, packages, cost_per_km, penalty_cost_per_minute, tournament_size=20)
+        return selection_function(population, tournament_size)
     else:
-        return selection_function(population, packages, cost_per_km, penalty_cost_per_minute)
+        return selection_function(population)
 
 
-def genetic_algorithm(packages : list[Package], cost_per_km: float, penalty_cost_per_minute: float,crossover_function, population_size=100, generations=10, mutation_rate=0.01):
+# Perguntar ao stor como é que devemos fazer a seleção dos pais da próxima geração
+# 2 childs por cada ou só uma?
+# podemos ter de alterar o crossover para simplificar
+# Também podemos adicionar o tamanho do torneio como argumento da função. Para já estamos a usar um default
+def genetic_algorithm(crossover_function, population_size=100, generations=1000, mutation_rate=0.01):
     population = generate_population(population_size)
+    best_solution = population[0]
+    best_cost = evaluate_solution(best_solution)
+
     for _ in range(generations):
         new_population = []
-        for i in range(1, population_size):
-            parent1 = random_selection(population, packages, cost_per_km, penalty_cost_per_minute)
-            parent2 = random_selection(population, packages, cost_per_km, penalty_cost_per_minute)
+        for solution in population:
+            cost = evaluate_solution(solution)
+            if cost < best_cost:
+                best_cost = cost
+                best_solution = solution
+            
+            parent1 = random_selection(population)
+            parent2 = random_selection(population)
             child1, child2 = crossover_function(parent1, parent2)
             if np.random.rand() < mutation_rate:
                 child1 = random_mutation(child1)
@@ -218,11 +282,48 @@ def genetic_algorithm(packages : list[Package], cost_per_km: float, penalty_cost
 
         population = new_population
 
-    solution = greatest_fit(population, packages, cost_per_km, penalty_cost_per_minute)
-    cost = evaluate_solution(solution, packages, cost_per_km, penalty_cost_per_minute)
-    return solution, cost
+    return best_solution, best_cost
 
 
+
+
+def print_packages_on_map(packages):
+    print (f"Number of packages: {num_packages}")
+    print (f"Size: {map_size}x{map_size}")
+    converted_map_size = 30
+    char_width = 2  # Adjust for the character width being half the character height
+
+    map_grid = [[' ' for _ in range(converted_map_size * char_width)] for _ in range(converted_map_size)]
+    map_grid[converted_map_size // 2][converted_map_size // 2 * char_width] = 'X'  # Starting point
+    for i, package in enumerate(packages):
+        x, y = package.coordinates_x, package.coordinates_y
+        x = int(x * (converted_map_size / map_size))  # Adjust x-coordinate to fit 30x30 map
+        y = int(y * (converted_map_size / map_size))  # Adjust y-coordinate to fit 30x30 map
+
+        if package.package_type == 'urgent':
+            symbol = bcolors.FAIL + str(i) + bcolors.ENDC
+        elif package.package_type    == 'fragile':
+            symbol = bcolors.OKBLUE + str(i) + bcolors.ENDC
+        else:
+            symbol = str(i)
+
+        if 0 <= x < converted_map_size and 0 <= y < converted_map_size:
+            map_grid[y][x * char_width] = symbol  # Adjusting for character width
+
+    # Top border
+    print(icons.TOP_LEFT + icons.HORIZONTAL * (converted_map_size * char_width) + icons.TOP_RIGHT)
+
+    for row in map_grid:
+        print(icons.VERTICAL, end='')
+        for cell in row:
+            print(cell, end='')
+        print(icons.VERTICAL)
+
+    # Bottom border
+    print(icons.BOTTOM_LEFT + icons.HORIZONTAL * (converted_map_size * char_width) + icons.BOTTOM_RIGHT)
+
+    # Legend
+    print(f"{bcolors.FAIL}Urgent{bcolors.ENDC} | {bcolors.OKBLUE}Fragile{bcolors.ENDC} | Normal")
 # Example: Generate a stream of 15 packages in a map of size 60x60
 package_stream = Package.generate_package_stream(num_packages, map_size)
 
@@ -237,15 +338,155 @@ columns=["Package", "Type", "CoordinatesX", "CoordinatesY", "Breaking Chance", "
 
 #print(df.to_string(index=False))
 print()
+test_packages = [Package('urgent', (56.97556558957435, 27.90045109824626)),
+                Package('fragile', (44.8203921010497, 45.239271577485084)),
+                Package('normal', (3.23756109561091, 21.1712638842833)),
+                Package('normal', (31.342549842605905, 43.79155490345392)),
+                Package('normal', (54.51149696067019, 55.917516798837994)),
+                Package('normal', (0.13982076073304261, 14.455164572152892)),
+                Package('fragile', (59.311394005015615, 52.17887506703184)),
+                Package('fragile', (43.07637979465492, 7.6826233225053775)),
+                Package('fragile', (43.662203998731016, 33.37818643749602)),
+                Package('urgent', (23.530958559725182, 3.3941997983349426))]
 
-solution, cost = genetic_algorithm(package_stream, cost_per_km, penalty_cost_per_minute, pmx_crossover)
-print(f"Best solution: {solution}")
-print(f"Best cost: {cost}")
-# solution1 = generate_random_solution()
-# solution2 = generate_random_solution()
-# print(solution1)
-# print(solution2)
+package_stream = test_packages
 
-# child_1, child_2 = pmx_crossover(solution1, solution2)
-# print (child_1)
-# print (child_2)
+show_map = True
+
+def print_menu_title_box(title):
+    print("\n\n")
+    print(icons.TOP_LEFT + icons.HORIZONTAL * (len(title) + 2) + icons.TOP_RIGHT)
+    print(icons.VERTICAL + " " + title + " " + icons.VERTICAL)
+    print(icons.BOTTOM_LEFT + icons.HORIZONTAL * (len(title) + 2) + icons.BOTTOM_RIGHT)
+    print()
+
+def hill_climbing_menu():
+    print_menu_title_box("Hill Climbing Menu")
+    iterations = int(input("Desired Number of iterations: "))
+
+    if show_map:
+        print_packages_on_map(package_stream)
+    solution, cost = hill_climbing(get_neighbor_solution, iterations)
+    print(f"Best Solution: {solution}")
+    print(f"Cost: {cost}")
+
+def simulated_annealing_menu():
+    print_menu_title_box("Simulated Annealing Menu")
+    iterations = int(input("Number of Iterations: "))
+    temperature = float(input("Initial temperature: "))
+    cooling = float(input("Cooling Rate: "))
+    if show_map:
+        print_packages_on_map(package_stream)
+    # Call simulated annealing algorithm function with specified parameters
+    solution = simulated_annealing(get_neighbor_solution, iterations, temperature, cooling)
+    print(f"Best Solution: {solution}")
+    print(f"Cost: {evaluate_solution(solution)}")
+
+def genetic_algorithm_menu():
+    print_menu_title_box("Genetic Algorithm Menu")
+    generations = int(input("Number of generations: "))
+    population_size = int(input("Population Size: "))
+    mutation_rate = float(input("Mutation Rate: "))
+
+    if show_map:
+        print_packages_on_map(package_stream)
+    # Call genetic algorithm function with specified parameters
+    solution, cost = genetic_algorithm(pmx_crossover, population_size, generations, mutation_rate)
+    print(f"Best Solution: {solution}")
+    print(f"Cost: {cost}")
+
+
+def settings_menu():
+    while True:
+        print_menu_title_box("Settings Menu")
+        print("1. Change Map Size")
+        print("2. Change Number of Packages")
+        print("3. Toggle Show Map")
+        print("4. Back to Main Menu")
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            change_map_size()
+        elif choice == "2":
+            change_num_packages()
+        elif choice == "3":
+            toggle_show_map()
+        elif choice == "4":
+            print("Returning to Main Menu...")
+            break
+        else:
+            print("Invalid choice. Please enter a valid option.")
+
+def toggle_show_map():
+    global show_map
+    show_map = not show_map
+    print(f"Show map updated to {show_map}.")
+
+def change_map_size():
+    global map_size
+    new_size = int(input("Enter the new map size: "))
+    if new_size > 0:
+        map_size = new_size
+        print("Map size updated successfully.")
+    else:
+        print("Invalid map size. Please enter a positive integer.")
+
+def change_num_packages():
+    global num_packages
+    new_packages = int(input("Enter the new number of packages: "))
+    if new_packages > 0:
+        num_packages = new_packages
+        print("Number of packages updated successfully.")
+    else:
+        print("Invalid number of packages. Please enter a positive integer.")
+
+def display_menu():
+    print_menu_title_box("Main Menu")
+    print("Welcome to the Package Delivery System:")
+    print("1. Run Hill Climbing Algorithm")
+    print("2. Run Simulated Annealing Algorithm")
+    print("3. Run Genetic Algorithm")
+    print("4. Settings")
+    print("5. Exit")
+
+def main():
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            hill_climbing_menu()
+        elif choice == "2":
+            simulated_annealing_menu()
+        elif choice == "3":
+            genetic_algorithm_menu()
+        elif choice == "4":
+            settings_menu()
+        elif choice == "5":
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice. Please enter a valid option.")
+
+if __name__ == "__main__":
+    main()
+
+
+# print_packages_on_map(package_stream)
+
+# Test Hill Climbing
+# print("Hill Climbing:")
+# solution, cost = hill_climbing(get_neighbor_solution, iterations=1000000)
+
+# Test SA
+#solution, cost = simulated_annealing(get_neighbor_solution)
+
+# Test Genetic
+#for _ in range(5):
+#    solution, cost = genetic_algorithm(pmx_crossover, generations=1000, population_size=50)
+#    print(f"Best solution: {solution}")
+#    print(f"Best cost: {cost}")
+
+
+# print(f"Best solution: {solution}")
+# print(f"Best cost: {cost}")
